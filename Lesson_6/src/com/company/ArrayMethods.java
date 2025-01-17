dd107aadd8fa4a1deca77fbcb3684c41291320f8
jongjong228 <kep.02@inbox.ru>
2025-01-17 14:19:56 +0300

    Lesson_6 is finnaly changed

--- Lesson_6/src/com/company/ArrayMethods.java
+++ Lesson_6/src/com/company/ArrayMethods.java
@@ -4,25 +4,12 @@ public class ArrayMethods {
 
     private static int sumOfElement;
 
-    public static void checkArraySize(String[][] array){
-        try{
-            int i = array.length;
-            int j;
-            if (i != 0) {
-                j = array[0].length;
-            } else {
-                System.out.println("Измените размерность массива!");
-                return;
-            }
-            if (i != 4 || j != 4) throw  new MyArraySizeException("Размерность не удовлетворяет условию");
-        }
-        catch (MyArraySizeException ex){
-            System.out.println(ex.getMessage());
-        }
-    }
-
     public static int getSumOfElements(String[][] array){
         try{
+            if (array.length !=4 || array[0].length != 4)
+            {
+                throw new MyArraySizeException("Неправильная размерность массива!");
+            }
           sumOfElement = 0;
             for (int i = 0; i < array.length; i++) {
                 for (int j = 0; j < array[0].length; j++) {
@@ -35,7 +22,7 @@ public class ArrayMethods {
             }
           return sumOfElement;
         }
-        catch(MyArrayDataException ex){
+        catch(MyArrayDataException | MyArraySizeException ex){
             System.out.println(ex.getMessage());
             return sumOfElement;
         }
--- Lesson_6/src/com/company/Main.java
+++ Lesson_6/src/com/company/Main.java
@@ -4,15 +4,13 @@ public class Main {
 
     public static void main(String[] args) throws MyArrayDataException {
 
-        String[][] first = new String[3][4];
-        ArrayMethods.checkArraySize(first);
-        String[][] second = new String[4][3];
-        ArrayMethods.checkArraySize(second);
-        String[][] third = new String[3][3];
-        ArrayMethods.checkArraySize(third);
-        String[][] fourth = new String[4][4];
-        ArrayMethods.checkArraySize(fourth);
-        fourth = new String[][]{{"1.1","мясо","3","4"},{"1","2","3","4"},{"1","2","3","4"},{"1","2","3","4"}};
+        String[][] first = new String[][]{{"1.1","мясо","3","4"},{"1","2","3","4"},{"1","2","3","4"}};
+        System.out.println(ArrayMethods.getSumOfElements(first));
+        String[][] second = new String[][]{{"1.1","мясо","3"},{"1","2","3"},{"1","2","3"},{"1","2","3"}};
+        System.out.println(ArrayMethods.getSumOfElements(second));
+        String[][] third = new String[][]{{"1.1","мясо","3"},{"1","2","3"},{"1","2","4"}};
+        System.out.println(ArrayMethods.getSumOfElements(third));
+        String[][] fourth = new String[][]{{"1.1","мясо","3","4"},{"1","2","3","4"},{"1","2","3","4"},{"1","2","3","4"}};
         System.out.println(ArrayMethods.getSumOfElements(fourth));
         fourth = new String[][]{{"1.1","20","3","4"},{"1","2","3","4"},{"1","2","3","4"},{"1","2","3","4"}};
         System.out.println(ArrayMethods.getSumOfElements(fourth));
